0ab836f77c3cecd903d50ef1dfe118847192b3aa
JunCham <573373910@qq.com>
2021-04-20 22:19:13 +0800

    deploy

---
+++ precache-manifest.4558392911c5b6a87f88e4300c05c8a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96bacbaaa041e45abeb7",
+    "url": "/Money-vue-website/css/app.a304df19.css"
+  },
+  {
+    "revision": "e152c9928c07699e3b28dcb640a6000b",
+    "url": "/Money-vue-website/index.html"
+  },
+  {
+    "revision": "96bacbaaa041e45abeb7",
+    "url": "/Money-vue-website/js/app.a3d78758.js"
+  },
+  {
+    "revision": "b4860003a6408af5ea98",
+    "url": "/Money-vue-website/js/chunk-vendors.e1c6fa5a.js"
+  },
+  {
+    "revision": "2389b9051ff3dfa899176100d61983f5",
+    "url": "/Money-vue-website/manifest.json"
+  },
+  {
+    "revision": "4f527293458610eae8d86e95202d0f51",
+    "url": "/Money-vue-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Money-vue-website/robots.txt"
+  }
+]);
